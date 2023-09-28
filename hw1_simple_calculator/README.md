782ea15f987b5073ed5cedb2f8c5189939eee7cb
EdwardLeeMacau <b05901119@ntu.edu.tw>
2023-09-28 20:25:46 +0800

    Impl add, sub, mul, div, pow and factorial

---
+++ hw1_simple_calculator/README.md
@@ -0,0 +1,8 @@
+# Hand notes
+
+## References
+
+- [RISC-V Spec v2.2](https://riscv.org/wp-content/uploads/2017/05/riscv-spec-v2.2.pdf)
+  - See page 121 => Register convention
+  - See page 122 => Frequently used instructions
+- [Jupiter Assembler Ecalls](https://jupitersim.gitbook.io/jupiter/assembler/ecalls#exit)
--- hw1_simple_calculator/r11922001.s
+++ hw1_simple_calculator/r11922001.s
@@ -2,6 +2,7 @@
 
 .rodata
     division_by_zero: .string "division by zero"
+    ops: .word op_add, op_sub, op_mul, op_div, op_min, op_pow, op_factorial
 
 .text
 __start:
@@ -17,11 +18,15 @@ __start:
     li a0, 5
     ecall
     mv s2, a0
-
-###################################
-#  TODO: Develop your calculator  #
-#                                 #
-###################################
+    # Optain the addr of operation
+    # int (*op)(int, int) = &ops[s1];
+    la t0, ops
+    slli t1, s1, 2
+    add t1, t1, t0
+    lw t1, 0(t1)
+    # Invoke the operation
+    # int result = op(s0, s2);
+    jr t1
 
 output:
     # Output the result
@@ -39,3 +44,53 @@ division_by_zero_except:
     la a1, division_by_zero
     ecall
     jal zero, exit
+
+op_add:
+    add s3, s0, s2
+    jal zero, output
+
+op_sub:
+    sub s3, s0, s2
+    jal zero, output
+
+op_mul:
+    mul s3, s0, s2
+    jal zero, output
+
+op_div:
+    beqz s2, division_by_zero_except
+    div s3, s0, s2
+    jal zero, output
+
+op_min:
+    # #define min(a, b) ((a) < (b) ? (a) : (b))
+    slt t0, s0, s2
+    bnez t0, op_min_true
+    mv s3, s2
+    j op_min_end
+op_min_true:
+    mv s3, s0
+op_min_end:
+    jal zero, output
+
+op_pow:
+    addi t0, zero, 1
+op_pow_loop:
+    beqz s2, op_pow_end
+    mul t0, t0, s0
+    addi s2, s2, -1
+    j op_pow_loop
+op_pow_end:
+    mv s3, t0
+    jal zero, output
+
+op_factorial:
+    addi t0, zero, 1
+op_factorial_loop:
+    beqz s0, op_factorial_end
+    mul t0, t0, s0
+    addi s0, s0, -1
+    j op_factorial_loop
+op_factorial_end:
+    mv s3, t0
+    jal zero, output
