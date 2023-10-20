4bb809b9922c6c349734f3e8230b199b050a10d0
EdwardLeeMacau <b05901119@ntu.edu.tw>
2023-10-20 10:55:22 +0800

    Draft Control, Mux32 and SignExtend

--- lab1_single_cycle_cpu/code/src/Control.v
+++ lab1_single_cycle_cpu/code/src/Control.v
@@ -1,6 +1,27 @@
 module Control
 (
-
+    opcode,
+    ALUOp,
+    ALUSrc,
+    RegWrite,
 );
+
+// Interface
+input [6:0] opcode;
+output [1:0] ALUOp;
+output ALUSrc;
+output RegWrite;
+
+// Spec:
+// Only 0b'0110011 (R-type) or 0b'0010011 (I-type) is expected to be used in this lab.
+//
+// ALUOp: not specified
+// ALUSrc is determined by opcode[5].
+// RegWrite is always 1.
+
+// Implementation
+assign ALUOp = 0;
+assign ALUSrc = ~opcode[5];
+assign RegWrite = 1;
 
 endmodule
